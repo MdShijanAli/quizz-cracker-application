cfa01b91d81f9c8c8b42ca13c92f393485af2b4f
FreelancerShijan <freelancershijan@gmail.com>
2022-10-10 20:57:56 +0600

    header completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../../images/logo.png';
+import './Header.css'
 
 const Header = () => {
     return (
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/topics">Topics</Link>
-            <Link to="/statics">Statics</Link>
-            <Link to="/blog">Blog</Link>
-            <Link to="/contact">Contact</Link>
+        <nav className='header'>
+            <div>
+                <img src={logo} alt="" />
+            </div>
+            <div>
+                <Link to="/">Home</Link>
+                <Link to="/topics">Topics</Link>
+                <Link to="/statics">Statics</Link>
+                <Link to="/blog">Blog</Link>
+                <Link to="/contact">Contact</Link>
+            </div>
         </nav>
     );
 };
